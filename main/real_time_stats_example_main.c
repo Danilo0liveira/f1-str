0d754b512c57eaa70b0e4af85c667fd6e12ea296
Danilo0liveira <danilo.melo@ee.ufcg.edu.br>
2026-03-17 18:29:12 -0300

    removed template functions and changed the log TAG

--- main/real_time_stats_example_main.c
+++ main/real_time_stats_example_main.c
@@ -37,7 +37,6 @@
 #define START_CHEAT_BUTTON GPIO_NUM_25
 #define FORCE_FAULT_BUTTON GPIO_NUM_27
 
-static char task_names[NUM_OF_SPIN_TASKS][configMAX_TASK_NAME_LEN];
 
 static SemaphoreHandle_t sync_spin_task;
 static SemaphoreHandle_t sync_stats_task;
@@ -67,126 +66,10 @@ static SemaphoreHandle_t xStartButtonPressed;
  *  - ESP_ERR_INVALID_SIZE  Insufficient array size for uxTaskGetSystemState. Trying increasing ARRAY_SIZE_OFFSET
  *  - ESP_ERR_INVALID_STATE Delay duration too short
  */
-static esp_err_t print_real_time_stats(TickType_t xTicksToWait)
-{
-    TaskStatus_t *start_array = NULL, *end_array = NULL;
-    UBaseType_t start_array_size, end_array_size;
-    configRUN_TIME_COUNTER_TYPE start_run_time, end_run_time;
-    esp_err_t ret;
-
-    //Allocate array to store current task states
-    start_array_size = uxTaskGetNumberOfTasks() + ARRAY_SIZE_OFFSET;
-    start_array = malloc(sizeof(TaskStatus_t) * start_array_size);
-    if (start_array == NULL) {
-        ret = ESP_ERR_NO_MEM;
-        goto exit;
-    }
-    //Get current task states
-    start_array_size = uxTaskGetSystemState(start_array, start_array_size, &start_run_time);
-    if (start_array_size == 0) {
-        ret = ESP_ERR_INVALID_SIZE;
-        goto exit;
-    }
-
-    vTaskDelay(xTicksToWait);
-
-    //Allocate array to store tasks states post delay
-    end_array_size = uxTaskGetNumberOfTasks() + ARRAY_SIZE_OFFSET;
-    end_array = malloc(sizeof(TaskStatus_t) * end_array_size);
-    if (end_array == NULL) {
-        ret = ESP_ERR_NO_MEM;
-        goto exit;
-    }
-    //Get post delay task states
-    end_array_size = uxTaskGetSystemState(end_array, end_array_size, &end_run_time);
-    if (end_array_size == 0) {
-        ret = ESP_ERR_INVALID_SIZE;
-        goto exit;
-    }
-
-    //Calculate total_elapsed_time in units of run time stats clock period.
-    uint32_t total_elapsed_time = (end_run_time - start_run_time);
-    if (total_elapsed_time == 0) {
-        ret = ESP_ERR_INVALID_STATE;
-        goto exit;
-    }
-
-    printf("| Task | Run Time | Percentage\n");
-    //Match each task in start_array to those in the end_array
-    for (int i = 0; i < start_array_size; i++) {
-        int k = -1;
-        for (int j = 0; j < end_array_size; j++) {
-            if (start_array[i].xHandle == end_array[j].xHandle) {
-                k = j;
-                //Mark that task have been matched by overwriting their handles
-                start_array[i].xHandle = NULL;
-                end_array[j].xHandle = NULL;
-                break;
-            }
-        }
-        //Check if matching task found
-        if (k >= 0) {
-            uint32_t task_elapsed_time = end_array[k].ulRunTimeCounter - start_array[i].ulRunTimeCounter;
-            uint32_t percentage_time = (task_elapsed_time * 100UL) / (total_elapsed_time * CONFIG_FREERTOS_NUMBER_OF_CORES);
-            printf("| %s | %"PRIu32" | %"PRIu32"%%\n", start_array[i].pcTaskName, task_elapsed_time, percentage_time);
-        }
-    }
-
-    //Print unmatched tasks
-    for (int i = 0; i < start_array_size; i++) {
-        if (start_array[i].xHandle != NULL) {
-            printf("| %s | Deleted\n", start_array[i].pcTaskName);
-        }
-    }
-    for (int i = 0; i < end_array_size; i++) {
-        if (end_array[i].xHandle != NULL) {
-            printf("| %s | Created\n", end_array[i].pcTaskName);
-        }
-    }
-    ret = ESP_OK;
-
-exit:    //Common return path
-    free(start_array);
-    free(end_array);
-    return ret;
-}
-
-static void spin_task(void *arg)
-{
-    xSemaphoreTake(sync_spin_task, portMAX_DELAY);
-    while (1) {
-        //Consume CPU cycles
-        for (int i = 0; i < SPIN_ITER; i++) {
-            __asm__ __volatile__("NOP");
-        }
-        vTaskDelay(pdMS_TO_TICKS(100));
-    }
-}
-
-static void stats_task(void *arg)
-{
-    xSemaphoreTake(sync_stats_task, portMAX_DELAY);
-
-    //Start all the spin tasks
-    for (int i = 0; i < NUM_OF_SPIN_TASKS; i++) {
-        xSemaphoreGive(sync_spin_task);
-    }
-
-    //Print real time stats periodically
-    while (1) {
-        printf("\n\nGetting real time stats over %"PRIu32" ticks\n", STATS_TICKS);
-        if (print_real_time_stats(STATS_TICKS) == ESP_OK) {
-            printf("Real time stats obtained\n");
-        } else {
-            printf("Error getting real time stats\n");
-        }
-        vTaskDelay(pdMS_TO_TICKS(1000));
-    }
-}
 
 // tags for logging
 static const char *TAG = "SENSOR_TASK";
-static const char *TAG2 = "DEBUGS BUTTON";
+static const char *TAG2 = "INJC_TASK";
 
 // variables to maniuplate the injection, jitter and the cheat start up flag
 int injection_value = INJECTION_STANDARD_VALUE;
